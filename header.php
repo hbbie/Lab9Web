dc46a631873227f8eccb13c9fde9ff9cf1f939dc
hbbie <nflxxy9@gmail.com>
2025-11-28 10:50:55 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+</head>
+
+<body>
+<div class="container">
+
+<header class="app-header">
+    <h1>PHP Modular</h1>
+</header>
+
+<div class="nav-table-container">
+    <table class="nav-table">
+        <tr>
+            <td><a href="index.php?page=home">Beranda </a></td>
+            <td><a href="index.php?page=about">Tentang</a></td>
+            <td><a href="index.php?page=user/list">Data Barang</a></td>
+            <td><a href="index.php?page=kontak">Kontak</a></td>
+        </tr>
+    </table>
+</div>
+<div class="content">
