da081273ed29f931b8234271288c75220ea98a36
hbbie <nflxxy9@gmail.com>
2025-11-28 10:46:54 +0700

    Create kontak.php

---
+++ pages/kontak.php
@@ -0,0 +1,10 @@
+<h2>Kontak</h2>
+
+<p>Silakan hubungi kami melalui informasi berikut:</p>
+
+<div class="contact-box">
+    <p><strong>Telepon:</strong> 0878-0011-6034</p>
+    <p><strong>Email:</strong> dhaninaufalhabibie@gmail.com</p>
+    <p><strong>Website:</strong> https://linktree/habibie</p>
+    <p><strong>Alamat:</strong> Bandung</p>
+</div>
